898e9f3e3f9d4955963c390a5c9fb9f6da49ff76
dylancolin <varunmaniar11@gmail.com>
2016-11-05 11:53:11 +0530

    made changes5 change_simple_db4

--- cust_init_home.php
+++ cust_init_home.php
@@ -104,8 +104,8 @@
 								<div class="col-sm-6">
 									<h1><span>E</span>thnic</h1>
 									<h2></h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-									<button type="button" class="btn btn-default get">Get it now</button>
+									<p>Bringuing Technic To Ethnic</p>
+									<!--<button type="button" class="btn btn-default get">Get it now</button>-->
 								</div>
 								<div class="col-sm-6">
 									<img src="images/home/home1.jpg" class="girl img-responsive" alt="" />
@@ -115,8 +115,8 @@
 								<div class="col-sm-6">
 									<h1><span>E</span>thnic</h1>
 									<h2></h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-									<button type="button" class="btn btn-default get">Get it now</button>
+									<p>Bringuing Technic To Ethnic</p>
+									<!--<button type="button" class="btn btn-default get">Get it now</button>-->
 								</div>
 								<div class="col-sm-6">
 									<img src="images/home/home2.jpg" class="girl img-responsive" alt="" />
@@ -127,8 +127,8 @@
 								<div class="col-sm-6">
 									<h1><span>E</span>thnic</h1>
 									<h2></h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-									<button type="button" class="btn btn-default get">Get it now</button>
+									<p>Bringuing Technic To Ethnic</p>
+									<!--<button type="button" class="btn btn-default get">Get it now</button>-->
 								</div>
 								<div class="col-sm-6">
 									<img src="images/home/home3.jpg" class="girl img-responsive" alt="" />
